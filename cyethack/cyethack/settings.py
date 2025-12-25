d88dcfd28cdcad2cba55cff3dc62f6453858d323
Nitish-wtv <nitish.kumar@wtvision.in>
2025-12-25 18:44:14 +0530

    updated settings

--- cyethack/cyethack/settings.py
+++ cyethack/cyethack/settings.py
@@ -25,10 +25,12 @@ load_dotenv(BASE_DIR / ".env", verbose=True)
 #SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-^-==xbk!l6)9&$o)*6t-faloo5rnvy+t2@=*#+3*2ejj1xc^@7')
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'threat.User'
+
+
 
 # Application definition
 
@@ -51,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -169,6 +172,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
